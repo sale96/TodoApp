f355d55d402d800042af595244fc6f2c22b45e0f
Aleksandar <aca801@gmail.com>
2020-10-02 16:58:23 +0200

    Create UseCaseExecutor

---
+++ TodoApp.Application/UseCaseExecutor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoApp.Application
+{
+    public class UseCaseExecutor
+    {
+        public UseCaseExecutor()
+        {
+        }
+
+        public void ExecuteCommand<TRequest>(
+            ICommand<TRequest> command,
+            TRequest request)
+        {
+            command.Execute(request);
+        }
+    }
+}
