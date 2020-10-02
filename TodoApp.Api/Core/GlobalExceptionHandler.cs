48350f9831e425a044853a53b4003af8fa8f6017
Aleksandar <aca801@gmail.com>
2020-10-02 18:45:53 +0200

    Add Api global exception handler

---
+++ TodoApp.Api/Core/GlobalExceptionHandler.cs
@@ -0,0 +1,67 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Api.Core
+{
+    public class GlobalExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch(Exception ex)
+            {
+                httpContext.Response.ContentType = "application/json";
+                object response = null;
+                var statusCode = StatusCodes.Status500InternalServerError;
+
+                switch(ex)
+                {
+                    case ValidationException validationException:
+                        statusCode = StatusCodes.Status422UnprocessableEntity;
+                        response = new
+                        {
+                            message = "Failed due to validation errors.",
+                            errors = validationException.Errors.Select(x => new
+                            {
+                                x.PropertyName,
+                                x.ErrorMessage
+                            })
+                        };
+                        break;
+                    default:
+                        statusCode = StatusCodes.Status500InternalServerError;
+                        response = new
+                        {
+                            message = ex.Message
+                        };
+                        break;
+                }
+
+                httpContext.Response.StatusCode = statusCode;
+
+                if (response != null)
+                {
+                    await httpContext.Response.WriteAsync(JsonConvert.SerializeObject(response));
+                    return;
+                }
+
+                await Task.FromResult(httpContext.Response);
+            }
+        }
+    }
+}
