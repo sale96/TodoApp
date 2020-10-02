545c1be78d5673afa720e907f11304e2db7687cb
Aleksandar <aca801@gmail.com>
2020-10-02 18:28:44 +0200

    Add create todo validator using FluentValidation

---
+++ TodoApp.Implementation/Validators/CreateTodoValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.Application.DataTransfer;
+using FluentValidation;
+using TodoApp.DataAccess;
+using System.Linq;
+
+namespace TodoApp.Implementation.Validators
+{
+    public class CreateTodoValidator : AbstractValidator<TodoDto>
+    {
+        public CreateTodoValidator(TodoAppContext context)
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .Must(name => !context.Todos.Any(t => t.Name == name))
+                .WithMessage("Todo must be unique!");
+        }
+    }
+}
