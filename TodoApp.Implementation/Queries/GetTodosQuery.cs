23f3a66d09bb9f68bdab38c467c566d71199c06e
Aleksandar <aca801@gmail.com>
2020-10-02 21:47:49 +0200

    Add GetTodosQuery implementation

---
+++ TodoApp.Implementation/Queries/GetTodosQuery.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TodoApp.Application.DataTransfer;
+using TodoApp.Application.Queries;
+using TodoApp.Application.Queries.Todo;
+using TodoApp.Application.Searches;
+using TodoApp.DataAccess;
+
+namespace TodoApp.Implementation.Queries
+{
+    public class GetTodosQuery : IGetTodosQuery
+    {
+        private readonly TodoAppContext _context;
+
+        public GetTodosQuery(TodoAppContext context)
+        {
+            _context = context;
+        }
+
+        public int Id => 4;
+
+        public string Name => "Get all todos query";
+
+        public PagedResponse<TodoDto> Execute(TodoSearch search)
+        {
+            var query = _context.Todos.AsQueryable();
+
+            if (!string.IsNullOrEmpty(search.Name) || !string.IsNullOrWhiteSpace(search.Name))
+                query = query.Where(x => x.Name.ToLower().Contains(search.Name));
+
+            var skipCount = search.PerPage * (search.Page - 1);
+
+            var response = new PagedResponse<TodoDto>
+            {
+                CurrentPage = search.Page,
+                ItemsPerPage = search.PerPage,
+                TotalCount = query.Count(),
+                Items = query.Skip(skipCount).Take(search.PerPage).Select(x =>
+                    new TodoDto
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        IsFinished = x.IsFinished,
+                        Status = x.Status
+                    }).ToList()
+            };
+
+            return response;
+        }
+    }
+}
