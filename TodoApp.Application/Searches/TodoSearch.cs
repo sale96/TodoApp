77fa4762233bb70440634bdeb31a1d32fcb91348
Aleksandar <aca801@gmail.com>
2020-10-02 21:36:58 +0200

    Add Search by name for Todos

---
+++ TodoApp.Application/Searches/TodoSearch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.Application.Queries;
+
+namespace TodoApp.Application.Searches
+{
+    public class TodoSearch : PagedSearch
+    {
+        public string Name { get; set; }
+    }
+}
