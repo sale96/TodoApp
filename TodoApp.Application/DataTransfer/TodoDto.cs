ada31c05ba766745eeed1241cb2ee3a046a81241
Aleksandar <aca801@gmail.com>
2020-10-02 18:18:49 +0200

    Add logic for creating todos

--- TodoApp.Application/DataTransfer/TodoDto.cs
+++ TodoApp.Application/DataTransfer/TodoDto.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TodoApp.Domain;
 
 namespace TodoApp.Application.DataTransfer
 {
     public class TodoDto : BaseDto
     {
-
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public TodoStatus Status { get; set; }
     }
 }
---
+++ TodoApp.Implementation/Commands/TodoCommands/AddTodoCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.Application.Commands.Todo;
+using TodoApp.Application.DataTransfer;
+using TodoApp.DataAccess;
+using TodoApp.Domain;
+
+namespace TodoApp.Implementation.Commands.TodoCommands
+{
+    public class AddTodoCommand : IAddTodoCommand
+    {
+        private readonly TodoAppContext _context;
+        public AddTodoCommand(TodoAppContext context)
+        {
+            _context = context;
+        }
+        public int Id => 1;
+
+        public string Name => "Add todo command";
+
+        public void Execute(TodoDto request)
+        {
+            Todo todo = new Todo
+            {
+                Name = request.Name,
+                Description = request.Description,
+                Status = request.Status
+            };
+
+            _context.Todos.Add(todo);
+            _context.SaveChanges();
+        }
+    }
+}
