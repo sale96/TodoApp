fde09ea1b200d428c730d87d28d6bfd1fb60c395
Aleksandar <aca801@gmail.com>
2020-10-02 21:38:49 +0200

    Fix IGetTodosQuery interface parameter and return type

--- TodoApp.Application/Queries/Todo/IGetTodosQuery.cs
+++ TodoApp.Application/Queries/Todo/IGetTodosQuery.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 using TodoApp.Application.DataTransfer;
+using TodoApp.Application.Searches;
 
 namespace TodoApp.Application.Queries.Todo
 {
-    public interface IGetTodosQuery : IQuery<TodoDto>
+    public interface IGetTodosQuery : IQuery<TodoSearch, PagedResponse<TodoDto>>
     {
     }
 }
