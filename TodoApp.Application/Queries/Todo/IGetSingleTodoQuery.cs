9d515690527ac267861286493b3c4d5038cfb99e
Aleksandar <aca801@gmail.com>
2020-10-02 22:16:30 +0200

    Add GetSingleTodo query:

---
+++ TodoApp.Application/Queries/Todo/IGetSingleTodoQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.Application.DataTransfer;
+
+namespace TodoApp.Application.Queries.Todo
+{
+    public interface IGetSingleTodoQuery : IQuery<int, TodoDto>
+    {
+    }
+}
---
+++ TodoApp.Implementation/Queries/GetSingleTodoQuery.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.Application.DataTransfer;
+using TodoApp.Application.Exceptions;
+using TodoApp.Application.Queries.Todo;
+using TodoApp.DataAccess;
+using TodoApp.Domain;
+
+namespace TodoApp.Implementation.Queries
+{
+    public class GetSingleTodoQuery : IGetSingleTodoQuery
+    {
+        private readonly TodoAppContext _context;
+
+        public GetSingleTodoQuery(TodoAppContext context)
+        {
+            _context = context;
+        }
+
+        public int Id => 5;
+
+        public string Name => "Get single todo query";
+
+        public TodoDto Execute(int search)
+        {
+            var todo = _context.Todos.Find(search);
+
+            if (todo == null)
+                throw new EntityNotFoundException(search, typeof(Todo));
+
+            var dto = new TodoDto
+            {
+                Name = todo.Name,
+                Description = todo.Description,
+                IsFinished = todo.IsFinished,
+                Status = todo.Status,
+                Id = todo.Id
+            };
+
+            return dto;
+        }
+    }
+}
