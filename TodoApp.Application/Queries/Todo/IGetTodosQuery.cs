f426621aab01feb5658ff95aee2549daacc37e2a
Aleksandar <aca801@gmail.com>
2020-10-02 21:35:12 +0200

    Add Interface for getting all Todos, classes for PagedResponse

---
+++ TodoApp.Application/Queries/Todo/IGetTodosQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.Application.DataTransfer;
+
+namespace TodoApp.Application.Queries.Todo
+{
+    public interface IGetTodosQuery : IQuery<TodoDto>
+    {
+    }
+}
