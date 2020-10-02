882a30dcab35bb823ee8e1baf1b613f3770e138a
Aleksandar <aca801@gmail.com>
2020-10-02 19:04:20 +0200

    Add override to SaveChanges method so we can automaticly set some properties

--- TodoApp.DataAccess/TodoAppContext.cs
+++ TodoApp.DataAccess/TodoAppContext.cs
@@ -21,6 +21,28 @@ namespace TodoApp.DataAccess
             builder.ApplyConfiguration(new TodoConfiguration());
         }
 
+        public override int SaveChanges()
+        {
+            foreach(var entry in ChangeTracker.Entries())
+            {
+                if (entry.Entity is BaseEntity e)
+                {
+                    switch (entry.State)
+                    {
+                        case EntityState.Added:
+                            e.CreateAt = DateTime.Now;
+                            e.UpdatedAt = null;
+                            break;
+                        case EntityState.Modified:
+                            e.UpdatedAt = DateTime.Now;
+                            break;
+                    }
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
         public DbSet<Todo> Todos { get; set; }
     }
 }
