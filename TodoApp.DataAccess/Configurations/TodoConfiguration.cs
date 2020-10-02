2036435a619e19ceabd369ad3c829d76115c2d1d
Aleksandar <aca801@gmail.com>
2020-10-02 16:22:50 +0200

    Remove first migration, Add Todo Configuration

---
+++ TodoApp.DataAccess/Configurations/TodoConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.Domain;
+
+namespace TodoApp.DataAccess.Configurations
+{
+    public class TodoConfiguration : IEntityTypeConfiguration<Todo>
+    {
+        public void Configure(EntityTypeBuilder<Todo> builder)
+        {
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(p => p.IsFinished).HasDefaultValue(false);
+        }
+    }
+}
--- TodoApp.DataAccess/TodoAppContext.cs
+++ TodoApp.DataAccess/TodoAppContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TodoApp.DataAccess.Configurations;
 using TodoApp.Domain;
 
 namespace TodoApp.DataAccess
@@ -13,6 +14,11 @@ namespace TodoApp.DataAccess
             optionBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TodoApp;Integrated Security=True");
 
             base.OnConfiguring(optionBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new TodoConfiguration());
         }
 
         public DbSet<Todo> Todos { get; set; }
